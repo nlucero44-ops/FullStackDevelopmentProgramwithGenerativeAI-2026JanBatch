ef86f18061630508be69a989a7d8b8c2c819e549
Akash <akash300383@gmail.com>
2026-02-12 08:43:40 +0530

    done

--- FSD - Foundations of Front-end Development/Programs/JS OOPs concept/classStyleObjectCreation.js
+++ FSD - Foundations of Front-end Development/Programs/JS OOPs concept/classStyleObjectCreation.js
@@ -1,33 +1,48 @@
-// function style ie ES5 
-function Employee1() {
-    // property 
-    this.id=100;
-    this.name = "Bob"
-    // behaviour 
-    this.displayEmployee1= function() {
-        document.write("<br/> Display function part of ES5 style object creation")
-        document.write("<br/> id is "+this.id)
-        document.write("<br/> name is "+this.name)
+// // function style ie ES5 
+// function Employee1() {
+//     // property 
+//     this.id=100;
+//     this.name = "Bob"
+//     // behaviour 
+//     this.displayEmployee1= function() {
+//         document.write("<br/> Display function part of ES5 style object creation")
+//         document.write("<br/> id is "+this.id)
+//         document.write("<br/> name is "+this.name)
+//     }
+// }
+// let emp1 = new Employee1();
+// emp1.displayEmployee1();
+
+// // class style ie ES6
+// class Employee2 {
+//     // property without this, var as well as let. these variable part of object without this keyword  
+//     //this.id=101;
+//     //let id =101;
+//     //var id = 101;
+//     id =100;
+//     name = "Bob"
+//     // behaviour 
+//     displayEmployee2() {
+//         document.write("<br/> Display function part of ES6 style object creation")
+//         document.write("<br/> id is "+this.id);
+//         document.write("<br/> name is "+this.name)
+//     }
+// }
+
+// let emp2 = new Employee2();
+// emp2.displayEmployee2();
+
+// constructor examples 
+class Employee {
+
+    constructor() {
+        document.write("<br/> object created")
+    }
+    display() {
+        document.write("<br/> Display function part of Employee class")
     }
 }
-let emp1 = new Employee1();
-emp1.displayEmployee1();
 
-// class style ie ES6
-class Employee2 {
-    // property without this, var as well as let. these variable part of object without this keyword  
-    //this.id=101;
-    //let id =101;
-    //var id = 101;
-    id =100;
-    name = "Bob"
-    // behaviour 
-    displayEmployee2() {
-        document.write("<br/> Display function part of ES6 style object creation")
-        document.write("<br/> id is "+this.id);
-        document.write("<br/> name is "+this.name)
-    }
-}
-
-let emp2 = new Employee2();
-emp2.displayEmployee2();
+let emp1 = new Employee();
+let emp2 = new Employee();
+emp1.display();
