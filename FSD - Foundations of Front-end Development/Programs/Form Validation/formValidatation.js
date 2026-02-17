61f42d01e1e796313524c9ae3e8b9b077a3e1df0
Akash <akash300383@gmail.com>
2026-02-17 07:46:13 +0530

    done

--- FSD - Foundations of Front-end Development/Programs/Form Validation/formValidatation.js
+++ FSD - Foundations of Front-end Development/Programs/Form Validation/formValidatation.js
@@ -5,23 +5,37 @@ function validation() {
     let emailId = document.getElementById("emailId1").value;
     let password = document.getElementById("pass1").value;
     let emailPattern = /[a-z]+@[a-z]+[.]com/i
+
+    //let pattern = "//"
     if(emailId.length==0){
-        alert("EmailId required")
+        //alert("EmailId required")
+        document.getElementById("emailIdError").innerHTML="EmailId is required"
         return false
     }else if(!emailPattern.test(emailId)){
-        alert("emailId pattern must be match")
+        //alert("emailId pattern must be match")
+        document.getElementById("emailIdError").innerHTML="EmailId pattern must be match"
         return false
     }else if(password.length==0){
-        alert("Password required")
+        //alert("Password required")
+        document.getElementById("emailIdError").innerHTML=""
+        document.getElementById("passwordError").innerHTML="Password is required"
         return false;
     }else if(password.length<4){
-        alert("Min password required 4 character")
+        //alert("Min password required 4 character")
+         document.getElementById("emailIdError").innerHTML=""
+        document.getElementById("passwordError").innerHTML="Password must be min 4 character"
         return false;
     }else if(emailId=="admin@gmail.com" && password=="admin@123"){  // it must be present in server side tchnology
-        alert("successfully login")
+        document.getElementById("emailIdError").innerHTML=""
+        document.getElementById("passwordError").innerHTML=""
+        //alert("successfully login")
+        document.getElementById("msg").innerHTML="Successfully login"
         return true;
     }else {
-        alert("failure try once again")
+        document.getElementById("emailIdError").innerHTML=""
+        document.getElementById("passwordError").innerHTML=""
+        document.getElementById("msg").innerHTML="Failure try once again"
+        //alert("failure try once again")
         return false;
     }
 
