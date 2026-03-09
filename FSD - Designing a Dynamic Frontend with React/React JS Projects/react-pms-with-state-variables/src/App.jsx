4aac7417c263cfabe14a14609ef485b4a5b55947
Akash <akash300383@gmail.com>
2026-03-10 05:16:30 +0530

    done

---
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/react-pms-with-state-variables/src/App.jsx
@@ -0,0 +1,13 @@
+import ProductOperation from "./ProductOperation"
+
+function App() {
+ 
+
+  return (
+    <div>
+      <ProductOperation></ProductOperation>
+    </div>
+  )
+}
+
+export default App
---
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/react-pms-with-state-variables/src/ProductOperation.jsx
@@ -0,0 +1,11 @@
+function ProductOperation() {
+
+
+    return(
+        <div>
+            <h2>Product CRUD Operation</h2>
+        </div>
+    )
+}
+
+export default ProductOperation;
