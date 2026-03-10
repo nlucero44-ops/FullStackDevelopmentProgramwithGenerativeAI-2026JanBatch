642a6e1db08260fd1df0e29e1c34766904e5a494
Akash <akash300383@gmail.com>
2026-03-10 06:37:13 +0530

    done

--- FSD - Designing a Dynamic Frontend with React/React JS Projects/react-pms-with-state-variables/src/ProductOperation.jsx
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/react-pms-with-state-variables/src/ProductOperation.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 function ProductOperation() {
 const [products,setProducts]=useState([]);  // array of product object  
 const [product,setProduct]=useState({pid:"",pname:"",price:"",url:""}) // product object 
-let storeProduct = (event)=> {
+const [buttonMsg,setButtonMsg]=useState("Add Product");
+
+let storeOrUpdateProduct = (event)=> {
     console.log("Event fired");
     event.preventDefault();     // disable action default behaviour 
     console.log(product)
@@ -13,13 +15,24 @@ let storeProduct = (event)=> {
     }
     
     // find if record not present it return undefined else it return that record. 
-    let isPresent = products.find(p=>p.pid==product.pid);
-    if(isPresent==undefined){
-        setProducts([...products,product]); 
-    alert("Product added successfully")
-    }else {
-        alert("Product id must be unique")
-    }
+
+        if(buttonMsg=="Add Product"){
+
+                let isPresent = products.find(p=>p.pid==product.pid);
+                if(isPresent==undefined){
+                setProducts([...products,product]); 
+                alert("Product added successfully")
+                }else {
+                alert("Product id must be unique")
+                }
+                
+
+        }else {
+
+                alert("Ready to update")
+                setButtonMsg("Add Product");
+        }
+
 
     setProduct({pid:"",pname:"",price:"",url:""})
 }
@@ -31,10 +44,17 @@ let deleteProduct = (pid)=> {
     setProducts(filterProducts);    // update new product in products state variable. 
     alert("product deleted successfully")
 }
+
+let setToUpdateProduct = (updateProduct)=> {
+    //console.log(updateProduct);
+    setProduct(updateProduct);  // set updateProduct value to product state variable 
+    // using value we display old value in form. 
+    setButtonMsg("Update Product");
+}
     return(
         <div>
             <h2>Product CRUD Operation</h2>
-            <form onSubmit={storeProduct}>
+            <form onSubmit={storeOrUpdateProduct}>
                 <input type="text" name="pid" value={product.pid}
                 placeholder="Enter the Product id" 
                 onChange={(event)=>setProduct({...product,pid:event.target.value})}/>
@@ -55,7 +75,7 @@ let deleteProduct = (pid)=> {
                 onChange={(event)=>setProduct({...product,url:event.target.value})}/>
                 <br/>
 
-                <input type="submit" value="Store Product"/>
+                <input type="submit" value={buttonMsg}/>
             </form>
             <hr/>
             <table border="1">
@@ -67,6 +87,7 @@ let deleteProduct = (pid)=> {
                         <th>Price</th>
                         <th>Image</th>
                         <th>Delete</th>
+                        <th>Update</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -78,7 +99,11 @@ let deleteProduct = (pid)=> {
                             <td>{product.price}</td>
                             <td><img src={product.url} width="50px" height="50px"/></td>
                             <td>
-                            <input type="button" value="Delete"onClick={()=>deleteProduct(product.pid)}/>
+                            <input type="button" value="Delete" onClick={()=>deleteProduct(product.pid)}/>
+                            </td>
+
+                            <td>
+                            <input type="button" value="Update" onClick={()=>setToUpdateProduct(product)}/>
                             </td>
                         </tr>
                         )
